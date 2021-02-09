ded6ff05d66095cd4b5d1378696728ca1eaa7418
werednnbs <werednnbs@users.noreply.github.com>
2021-02-08 23:52:34 -0400

    cambiosv6

--- resources/views/sitio/prensa.blade.php
+++ resources/views/sitio/prensa.blade.php
@@ -4,26 +4,34 @@
     <div class="container container-lg">
         <div class="row">
             {{-- <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-00.jpg" alt="" /></div></div> --}}
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-01.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-02.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-03.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-04.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-05.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-06.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-07.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-08.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-09.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-10.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-11.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-12.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-13.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-14.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-15.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-16.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-17.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-18.jpg" alt="" /></div></div>
-            <div class="col-4 col-md-3"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-19.jpg" alt="" /></div></div>
 
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-01.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-02.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-03.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-12.jpg" alt="" /></div></div>
+            <div class="col-4">
+                <div class="row">
+                    <div class="col-12"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-06.jpg" alt="" /></div></div>
+                    <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-16.jpg" alt="" /></div></div>
+                </div>
+            </div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-15.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-08.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-09.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-10.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-11.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-13.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-14.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-17.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-18.jpg" alt="" /></div></div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-19.jpg" alt="" /></div></div>
+            <div class="col-4">
+                <div class="row">
+                    <div class="col-12"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-04.jpg" alt="" /></div></div>
+                    <div class="col-12"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-07.jpg" alt="" /></div></div>
+                </div>
+            </div>
+            <div class="col-4"><div class="item item-gallery"><img src="assets/images/prensa/img-prensa-05.jpg" alt="" /></div></div>
         </div>
     </div>
 </div>
