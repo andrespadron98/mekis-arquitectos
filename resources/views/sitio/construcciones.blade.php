b369bd49fcc2447a781147b205c9000f8cd93aaa
werednnbs <werednnbs@users.noreply.github.com>
2021-02-13 15:25:00 -0400

    cambioConstrucciones

--- resources/views/sitio/construcciones.blade.php
+++ resources/views/sitio/construcciones.blade.php
@@ -10,13 +10,13 @@
             <div class="row text-center">
                 <h2 class="py-4 text-center w-100">CHICUREO</h2>
                 <div class="position-relative w-100">
-                    <div class="sliderwide"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid"></div>
-                    <div class="sliderwide"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid"></div>
-                    <div class="sliderwide"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid"></div>
-                    <div class="sliderwide"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid"></div>
-                    <div class="sliderwide"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid"></div>
-                    <div class="sliderwide"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid"></div>
-                    <div class="sliderwide"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid"></div>
+                    <div class="sliderwide position-relative" style="height: 30rem;overflow: hidden;"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid position-absolute" style="left: 0;"></div>
+                    <div class="sliderwide position-relative" style="height: 30rem;overflow: hidden;"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid position-absolute" style="left: 0;"></div>
+                    <div class="sliderwide position-relative" style="height: 30rem;overflow: hidden;"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid position-absolute" style="left: 0;"></div>
+                    <div class="sliderwide position-relative" style="height: 30rem;overflow: hidden;"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid position-absolute" style="left: 0;"></div>
+                    <div class="sliderwide position-relative" style="height: 30rem;overflow: hidden;"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid position-absolute" style="left: 0;"></div>
+                    <div class="sliderwide position-relative" style="height: 30rem;overflow: hidden;"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid position-absolute" style="left: 0;"></div>
+                    <div class="sliderwide position-relative" style="height: 30rem;overflow: hidden;"><img src="assets/images/construcciones/img-chicureo-00-wide.png" class="img-fluid position-absolute" style="left: 0;"></div>
                     <div class="owl-page owl-carousel pt-1">
                         <div class="item"><img src="assets/images/construcciones/img-chicureo-00.png" class="img-fluid cursor-pointer" onclick="currentSlide(1)" alt=""></div>
                         <div class="item"><img src="assets/images/construcciones/img-chicureo-00.png" class="img-fluid cursor-pointer" onclick="currentSlide(2)" alt=""></div>
@@ -28,7 +28,7 @@
                     </div>
                 </div>
             </div>
-            <div class="row text-center">
+            {{-- <div class="row text-center">
                 <h2 class="py-4 text-center w-100">LA BARCA</h2>
                 <div class="position-relative w-100">
                     <div class="sliderwide2"><img src="assets/images/construcciones/img-barca-00-wide.png" class="img-fluid"></div>
@@ -48,7 +48,7 @@
                         <div class="item"><img src="assets/images/construcciones/img-barca-00.png" class="img-fluid cursor-pointer" onclick="currentSlide2(7)" alt=""></div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
